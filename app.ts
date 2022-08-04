1784a870118a562ad417d20d3c81d0cdd9b6ce75
Ti Jhae <jhaeti@gmail.com>
2022-08-04 14:58:17 +0000

    Learn the public and private modifiers

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
 // CLASS
 
+class ClassWithPublicProp {
+    private id :number=0;
+}
+
+const publicAccess = new ClassWithPublicProp()
+// publicAccess.id = 15 /* This yield an error when the private modifier is set on a property in a class */
+
+console.log(publicAccess)
