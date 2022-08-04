05dd53932b55571bfcf79eb4c456a1029df80b41
Ti Jhae <jhaeti@gmail.com>
2022-08-04 12:48:28 +0000

    Done with implementing interfaces with classed

--- app.ts
+++ app.ts
@@ -1,11 +1,33 @@
 // CLASSES
-class ClassA {
+
+interface IPrint {
+	print();
+}
+
+const object: IPrint = {
+    print:  () => {
+        console.log("From object")
+    }
+}
+function printClass(a: IPrint) {
+	a.print();
+}
+
+class ClassA implements IPrint {
     print() {
         console.log("ClassA.print()")
     }
 }
-class ClassB {
+class ClassB implements IPrint {
     print() {
         console.log("ClassB.print()")
     }
 }
+
+const classA = new ClassA()
+const classB = new ClassB()
+
+
+classA.print()
+classB.print()
+object.print()
