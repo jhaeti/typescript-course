e76950fd4faaabd29e969cf52bfff4e18070ef89
Ti Jhae <jhaeti@gmail.com>
2022-08-04 13:14:15 +0000

    Learning to type class modifiers

--- app.ts
+++ app.ts
@@ -1,16 +1,2 @@
-// CLASSES
-class ClassWithConstructor {
-	id: number;
-	name: string;
-	constructor(_id: number, _name: string) {
-		this.id = _id;
-		this.name = _name;
-	}
-	getName():void {
-		console.log(this.name);
-	}
-}
+// CLASS
 
-const classWithConstructor = new ClassWithConstructor(0, "apple");
-
-console.log(classWithConstructor.getName());
