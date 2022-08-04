609e10ba9ee9db4a9beba3ab46420d152692d5b0
Ti Jhae <jhaeti@gmail.com>
2022-08-04 11:48:09 +0000

    Done with iterfaces

--- app.ts
+++ app.ts
@@ -2,10 +2,13 @@ interface ComplexType {
     id: number;
     name: string;
 }
-
 const complexType: ComplexType = {
     id: 5,
     name: "Ti Jhae"
 }
 
-console.log(complexType)
+interface Point {readonly x: number; y: number; }
+const point: Point = { x: 2, y: 3 }
+// point.x = 5;    /* This does throw an error with readonly*/
+
+console.log(point)
