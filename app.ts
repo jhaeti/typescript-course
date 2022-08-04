714dd8655d4ce29c00256c9b911ffd88e5ff7792
Ti Jhae <jhaeti@gmail.com>
2022-08-04 13:13:26 +0000

    Done with class constructor typing

--- app.ts
+++ app.ts
@@ -1,2 +1,16 @@
 // CLASSES
+class ClassWithConstructor {
+	id: number;
+	name: string;
+	constructor(_id: number, _name: string) {
+		this.id = _id;
+		this.name = _name;
+	}
+	getName():void {
+		console.log(this.name);
+	}
+}
 
+const classWithConstructor = new ClassWithConstructor(0, "apple");
+
+console.log(classWithConstructor.getName());
